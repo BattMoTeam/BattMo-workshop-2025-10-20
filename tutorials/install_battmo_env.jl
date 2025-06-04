0e8255c13ddaed9d0a34d2a943bb6a6bdf524018
Lorena Hendrix <lorenahendrix163@gmail.com>
2025-06-04 11:23:32 +0200

    dev tutorials

--- tutorials/install_battmo_env.jl
+++ tutorials/install_battmo_env.jl
@@ -1,7 +1,7 @@
 using Pkg: Pkg
 
-Pkg.activate("tutorials") # Activates the environment in the current folder
+Pkg.activate(".") # Activates the environment in the current folder
 Pkg.instantiate()
-Pkg.add(url = "https://github.com/BattMoTeam/BattMo.jl", rev = "main")
+Pkg.add(url = "https://github.com/BattMoTeam/BattMo.jl", rev = "a52c1e31fb99e1e5d838f06ece4689bce85ba4cc")
 
 
