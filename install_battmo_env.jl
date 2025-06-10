890bb1e9fc708c884eae83c3105d31e16e359d27
Eibar Joel Flores Cedeño <eibar.flores.cedeno@sintef.no>
2025-06-10 11:23:09 +0200

    LangageServer installation added to install_battmo_env.jl

--- install_battmo_env.jl
+++ install_battmo_env.jl
@@ -7,6 +7,7 @@ Pkg.add("GLMakie")      # For plotting
 Pkg.add("Jutul")        # A multiphysics simulation framework for reservoir modeling and scientific computing
 Pkg.add("CSV")          # For reading and writing CSV files
 Pkg.add("DataFrames")   # Provides tabular data structures and data manipulation tools, similar to pandas
+Pkg.add("LanguageServer") # Helps VS Code run julia notebooks.
 
 
 
