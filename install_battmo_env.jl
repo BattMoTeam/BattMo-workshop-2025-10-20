14f1cbae04643a357370649f52b3785d617ba770
Eibar Joel Flores Cedeño <eibar.flores.cedeno@sintef.no>
2025-06-12 22:48:56 +0200

    updated battmo install to latest release

--- install_battmo_env.jl
+++ install_battmo_env.jl
@@ -2,7 +2,7 @@ using Pkg: Pkg
 
 Pkg.activate(".") # Activates the environment in the current folder
 Pkg.instantiate()
-Pkg.add(url = "https://github.com/BattMoTeam/BattMo.jl", rev = "a52c1e31fb99e1e5d838f06ece4689bce85ba4cc")
+Pkg.add("BattMo")
 Pkg.add("GLMakie")      # For plotting
 Pkg.add("Jutul")        # A multiphysics simulation framework for reservoir modeling and scientific computing
 Pkg.add("CSV")          # For reading and writing CSV files
