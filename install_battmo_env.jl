9d5addce72285d64c06c3d4702e87de324755d87
Lorena Hendrix <lorenahendrix163@gmail.com>
2025-10-17 16:06:26 +0200

    update

--- install_battmo_env.jl
+++ install_battmo_env.jl
@@ -3,7 +3,7 @@ using Pkg: Pkg
 Pkg.activate(".") # Activates the environment in the current folder
 Pkg.instantiate()
 Pkg.Registry.update()
-Pkg.add("BattMo")
+Pkg.add(PackageSpec(name = "BattMo", version = "0.2.3"))
 Pkg.add("GLMakie")      # For plotting
 Pkg.add("Jutul")        # A multiphysics simulation framework for reservoir modeling and scientific computing
 Pkg.add("CSV")          # For reading and writing CSV files
