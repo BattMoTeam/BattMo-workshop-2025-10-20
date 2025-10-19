2e6b58b7d6f839c097ab44a3710b393c0ed43079
Lorena Hendrix <lorenahendrix163@gmail.com>
2025-10-19 17:19:02 +0200

    everything in one notebook

--- install_battmo_env.jl
+++ install_battmo_env.jl
@@ -4,7 +4,7 @@ Pkg.activate(".") # Activates the environment in the current folder
 Pkg.instantiate()
 Pkg.Registry.update()
 Pkg.add(PackageSpec(name = "BattMo", version = "0.2.3"))
-Pkg.add("GLMakie")      # For plotting
+Pkg.add("WGLMakie")      # For plotting
 Pkg.add("Jutul")        # A multiphysics simulation framework for reservoir modeling and scientific computing
 Pkg.add("CSV")          # For reading and writing CSV files
 Pkg.add("DataFrames")   # Provides tabular data structures and data manipulation tools, similar to pandas
